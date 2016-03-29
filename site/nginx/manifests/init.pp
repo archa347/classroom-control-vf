24bea595e1cf4d23df6ab23ccaca832b35e8a609
Daniel G <daniel.r.gallegos@gmail.com>
2016-03-29 23:27:48 +0000

    added class to nginx module

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,28 @@
+class ngninx {
+  package { 'nginx':
+    ensure => present,
+  }
+  file { 'nginx.conf':
+    path    => '/etc/nginx/nginx.conf',
+    ensure  => present,
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  file { 'default.conf':
+    path    => '/etc/nginx/conf.d/default.conf',
+    ensure  => present,
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+  file { 'index.html':
+    path    => '/var/www/index.html',
+    ensure  => present,
+    source  => 'puppet:///modules/nginx/index.html',
+    require => Package['nginx'],
+  }
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => [File['nginx.conf'],File['default.conf']],
+  }
+}
