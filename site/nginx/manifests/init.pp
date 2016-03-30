9a73d36244384d405b7cf3173a3b629ec6c4002d
Daniel G <daniel.r.gallegos@gmail.com>
2016-03-30 16:01:53 +0000

    refactoring nginx module

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,31 +1,36 @@
 class nginx {
+  File {
+    ensure => present,
+    owner  => 'root',
+    mode   => '0664',
+  }
+
+  $nginx_conf='/etc/nginx/nginx.conf'
+  $default_conf='/etc/nginx/conf.d/default.conf'
+  $root_dir='/var/www'
+  $index="${root_dir}/index.html"
+
   package { 'nginx':
     ensure => present,
   }
-  file { 'nginx.conf':
-    path    => '/etc/nginx/nginx.conf',
-    ensure  => present,
+  file { $nginx_conf:
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
   }
-  file { 'default.conf':
-    path    => '/etc/nginx/conf.d/default.conf',
-    ensure  => present,
+  file { $default_conf:
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
   }
-  file { '/var/www':
+  file { $root_dir:
     ensure => directory,
   }
-  file { 'index.html':
-    path    => '/var/www/index.html',
-    ensure  => present,
+  file { $index:
     source  => 'puppet:///modules/nginx/index.html',
     require => [Package['nginx'],File['/var/www']],
   }
   service { 'nginx':
     ensure    => running,
     enable    => true,
-    subscribe => [File['nginx.conf'],File['default.conf']],
+    subscribe => [File[$nginx_conf],File[$default_conf]],
   }
 }
