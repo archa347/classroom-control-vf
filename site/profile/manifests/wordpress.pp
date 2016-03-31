ad4e6ad6eb2809ba4c610b4bb741fc0b8ecfa3a6
Daniel G <daniel.r.gallegos@gmail.com>
2016-03-31 21:18:18 +0000

    Added some more stuff to wordpress profile

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,13 +1,38 @@
 class profile::wordpress {
   ## Mysql Server
-
-  class { '::mysql::server': }
+  include '::mysql::server'
 
   ## Wordpress config
 
   ## Apache vhost config
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  apache::vhost { 'wordpress.archa347.puppetlabs.vm':
+    port    => '80',
+    docroot => '/var/www/wordpress',
+  }
 
   ##Local user and group for WP
+  group { 'wordpress':
+    ensure => present,
+  }
+
+  user { 'wordpress':
+    ensure  => present,
+    gid     => 'wordpress'
+    require => Group['wordpress']
+  }
+
 
   ##Host Entry
+
+  host { 'wordpress.archa347.puppetlabs.vm':
+    ip      => '127.0.0.1',
+    target  => '/etc/hosts',
+    comment => 'added via wordpress profile',
+    ensure  => 'present',
+  }
+                        }
 }
