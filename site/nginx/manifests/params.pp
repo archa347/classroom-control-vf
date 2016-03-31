fee74c287031896d878e984823e8eb5c4139a7fa
Daniel G <daniel.r.gallegos@gmail.com>
2016-03-31 18:27:20 +0000

    nginx params added

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,24 @@
+class nginx::params { 
+  case $::osfamily {
+    'redhat','debian': {
+      $package_name='nginx'
+      $nginx_user= $::osfamily ? {
+        'redhat'=> 'nginx',
+        'debian'=> 'www-data',
+      }
+      $config='/etc/nginx/'
+      $server_block="${config_dir}conf.d/"
+      $root='/var/www'
+      $log='/var/log/nginx/'
+    }
+    'windows' : {
+      $package_name='nginx-service'
+      $nginx_user='nobody'
+      $config='C:/ProgramData/nginx/'
+      $server_block="${config_dir}conf.d/"
+      $root='C:/ProgramData/nginx/html'
+      $log='C:/ProgramData/nginx/logs/'
+    }
+  }
+  $file_source='puppet:///modules/nginx'
+}
